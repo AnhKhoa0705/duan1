a448197d0395fb3d563f647d960faf15fad32375
VoPhat <vohongphat2892005@gmail.com>
2025-04-10 12:59:57 +0700

    hi

--- index.php
+++ index.php
@@ -1,25 +1,6 @@
 <?php
 session_start();
 
-// if (isset($_GET['mod'])){
-//     switch ($_GET['mod']){
-//         case 'product':
-//             include_once 'controllers/productController.php';
-//             break;
-//         case 'categories':
-//             include_once 'controllers/categoriesController.php';
-//             break;
-//         default:
-//             header(header: "Location: ?mod=page&act=home");
-//             break;
-
-//     }
-// }else{
-//     header(header: "Location: ?mod=page&act=home"); 
-// };
-
-// include_once "view/header.php";
-// include_once "view/footer.php";
 require_once 'view/header.php';
 // user
 
